ece669199ead1149844c915ea485908f5daf0f94
Davit Nahapetyan <dnahapet@gmail.com>
2023-07-05 21:59:46 -0400

    Created account view model component

---
+++ Bank-App/Screens/AccountSummaryScreen.swift
@@ -0,0 +1,20 @@
+//
+//  AccountSummaryScreen.swift
+//  Bank-App
+//
+//  Created by Davit Nahapetyan on 2023-07-05.
+//
+
+import SwiftUI
+
+struct AccountSummaryScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountSummaryScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountSummaryScreen()
+    }
+}
---
+++ Bank-App/View Models/AccountSummaryViewModel.swift
@@ -0,0 +1,55 @@
+//
+//  AccountSummaryViewModel.swift
+//  Bank-App
+//
+//  Created by Davit Nahapetyan on 2023-07-05.
+//
+
+import Foundation
+
+class AccountSummaryViewModel: ObservableObject {
+    private var _accountsModels: [Account] = [Account]()
+    @Published var accounts: [AccountViewModel] = [AccountViewModel]()
+
+    var total: Double {
+        self._accountsModels.map({$0.balance}).reduce(0, +)
+    }
+
+    func getAllAccounts() {
+        AccountService.shared.getAllAccounts { result in
+            switch result {
+                case .success(let accounts):
+                    if let accounts = accounts {
+                        self._accountsModels = accounts
+                        self.accounts = accounts.map(AccountViewModel.init)
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+class AccountViewModel {
+    let account: Account
+
+    init(account: Account) {
+        self.account = account
+    }
+
+    var name: String {
+        self.account.name
+    }
+
+    var id: String {
+        self.account.id
+    }
+
+    var accountType: String {
+        self.account.accountType.title
+    }
+
+    var balance: String {
+        self.account.balance.formatAsCurrency()
+    }
+}
